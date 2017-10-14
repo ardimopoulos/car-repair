d21e7c81de419d93fd4711cc03fb6cee9b9b3d79
d-elz <kwspoul@gmail.com>
2017-10-14 19:12:35 +0300

    nothing much to controller

--- src/main/java/com/carRepair/carRepair/web/LogInController.java
+++ src/main/java/com/carRepair/carRepair/web/LogInController.java
@@ -1,15 +1,20 @@
 package com.carRepair.carRepair.web;
 
+import com.carRepair.carRepair.Forms.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LogInController {
 
-    @RequestMapping(value="/" ,  method = RequestMethod.GET)
-    public String index(){
+    private static final String LOGIN_FORM = "loginForm";
 
+    @RequestMapping(value="/" ,  method = RequestMethod.GET)
+    public String index(Model model){
+
+        model.addAttribute(LOGIN_FORM, new LoginForm());
         return "login";
     }
 
