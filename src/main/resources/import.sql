e055053f80a11989ce52ee97eaab0c82afb1fcf7
delz <kwspoul@gmail.com>
2017-10-21 01:37:28 +0300

    some few repair imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,10 +6,19 @@ insert into member (address,firstname,lastname,vat,user_id)  values('Karpenisiou
 insert into member (address,firstname,lastname,vat,user_id)  values('Kavalas 12','Georgios','Georgiou',123345789,2);
 insert into member (address,firstname,lastname,vat,user_id)  values('Athinas 8','Akis','Dimopoulos',456852397,3);
 
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description1', '0', '11:45:00', false, '1');
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description2', '1', '10:00:00', true, '2');
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description3', '2', '08:45:00', false, '3');
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description4', '0', '17:00:00', true, '1');
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description5', '1', '19:15:00', false, '3');
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description6', '2', '12:45:00', true, '2');
-insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-19', 'This is a description7', '0', '12:30:00', false, '1');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description1', '0', '11:45:00', false, '1');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description2', '1', '10:00:00', true, '2');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description3', '2', '08:45:00', false, '3');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description4', '0', '17:00:00', true, '1');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description5', '1', '19:15:00', false, '3');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description6', '2', '12:45:00', true, '2');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description7', '0', '12:30:00', false, '1');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description8', '0', '11:45:00', false, '3');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description9', '0', '11:00:00', true, '2');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description10', '0', '08:00:00', false, '1');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description11', '1', '11:00:00', true, '3');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description12', '2', '09:15:00', false, '1');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description13', '1', '12:15:00', true, '2');
+insert into repair ( date, description, status, time, type, user_id) VALUES ('2017-10-21', 'This is a description14', '1', '12:30:00', false, '3');
+
+
