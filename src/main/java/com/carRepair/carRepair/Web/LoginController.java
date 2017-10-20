049ca78986bb8607ed40441b343523375c783b05
pantBarb <pantelosjoke@gmail.com>
2017-10-20 18:11:36 +0300

    Update New Service page and add css file.

--- src/main/java/com/carRepair/carRepair/Web/LoginController.java
+++ src/main/java/com/carRepair/carRepair/Web/LoginController.java
@@ -1,14 +1,9 @@
 package com.carRepair.carRepair.Web;
 
-import com.carRepair.carRepair.Domain.Member;
-import com.carRepair.carRepair.Domain.User;
 import com.carRepair.carRepair.Forms.LoginForm;
-import com.carRepair.carRepair.Repositories.MemberRepository;
-import com.carRepair.carRepair.Repositories.UserRepository;
 import com.carRepair.carRepair.Services.AccountServiceImpl;
 import com.carRepair.carRepair.Utilities.AppUtilities;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +17,6 @@ public class LoginController {
     private static final String LOGIN_FORM = "loginForm";
     private final static org.slf4j.Logger LOG = LoggerFactory.getLogger(AccountServiceImpl.class);
 
-    @Autowired
-    private UserRepository repo;
-    private MemberRepository repom;
-
     @RequestMapping(value = {"/login" , "/"}, method = RequestMethod.GET)
     public String getLoginView(Model model, @RequestParam(name = "error", required = false) String error, HttpServletRequest request) {
         String view = "/login";
@@ -38,6 +29,8 @@ public class LoginController {
         }
 
         if(!model.containsAttribute(LOGIN_FORM)){ model.addAttribute(LOGIN_FORM, new LoginForm()); }
+
+
 
         return view;
     }
